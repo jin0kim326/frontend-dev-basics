72ead84aab86cd1bda830d5e030a5e6bf5d9e2af
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-11-05 14:42:12 +0900

    js basic#2

--- javascipt-practice/basics/ex02.js
+++ javascipt-practice/basics/ex02.js
@@ -1,6 +1,6 @@
 /*
 변수와 데이터 타입
-[기본타입(primitive type)]
+[기본타입(primitive type), 유사객체]
 undefined
 number
 string
@@ -8,15 +8,33 @@ boolean
 
 [객체1] - object type
 1. new 생성자함수()  사용해서 생성
-       Number() -> object type
+       Number()  -> object type
        Boolean() -> object type
-       String() -> object type
-       Object() -> object type
-       Array() -> object type
-
+       String()  -> object type
+       Object()  -> object type
+       Array()   -> object type
+2. null
+3. {} (객체 리터럴, JSON)
+4. [] (배열 리터럴)
 
 [객체2] - function type
+1. new Function(...) 생성
+2. var f = function(){} 함수 리터럴1을 사용해서 생성
+3. function f() { } 함수 리터럴1을 사용해서 생성
+*/
 
+/*
+자바스크립트의 객체를 구분하는 또 다른 방법
+1. 내장(Built-In, Native, Engine) 객체
+   자바스크립트 엔진안에 미리 내장되어 있는 객체
+   Number, Boolean, Date, RegExp, ....   : 생성자 함수
+   parseInt, setTimeout, ...             : 일반 함수 
+2. 호스트 객체
+   자바스크립트 엔진이 임베디드된 환경(호스트)의 객체(자바스크립트 접근 가능)
+   호스트 환경이 브라우저라면: document, location, XmlHttpRequest, window, ....
+   호스트 환경이 노드(Node.JS)라면: os, http, fs, path  
+3. 사용자 객체
+   자바스크립트 엔진이 실행되면서 생성되는 객체들....
 */
 
 console.log("==== 원시 타입(Primitive Type) =================================");
@@ -60,3 +78,17 @@ var f2 = function (a, b) {
 
 console.log("f:" + typeof f);
 console.log("f2:" + typeof f2);
+
+console.log(
+  "==== 원시타입과 원시 타입을 Wrapper 객체는 구분없이 사용할 수 있다. ================================="
+);
+console.log(i + i2);
+console.log(s + s2);
+
+// 원시 타입에 메소드가 호출 될 때,
+// 임시 Wrapper 객체가 만들어져서 메소드 호출이 일어난 뒤 사라진다.(유사객체)
+console.log(
+  "==== 원시타입도 메소드 호출이 가능하다. ================================="
+);
+console.log(b.valueOf());
+console.log(b2.valueOf());
